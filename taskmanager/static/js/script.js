ab7e7df464af35ab9a134761e3f0574443e4dad2
Varinia Butcher <varinia_b@hotmail.com>
2024-07-06 14:14:58 +0000

    Add functionality add form for adding a task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialisation
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+  // sidenav initialisation
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm yyyy",
+    i18n: {done: "Select"}
   });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+});
